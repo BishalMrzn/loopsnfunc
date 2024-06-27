066f63c4fda268cbd4d93a75e9d5fec6ee6106ac
BishalMrzn <112357355+BishalMrzn@users.noreply.github.com>
2024-06-27 12:59:32 +0545

    Add files via upload

---
+++ Practise Set 3/question1.js
@@ -0,0 +1,20 @@
+// let marks={
+//     harry:98,
+//     rohan:70,
+//     aakash:7,
+// }
+// // console.log(obj)
+// // console.log(obj["harry"])
+// for(let i=0;i<Object.keys(marks).length;i++){
+//     console.log("The marks of"+Object.keys(marks)[i]+"are"+marks[Object.keys(marks)[i]])
+
+// }
+let marks={
+    bishal:80,
+    raj:70,
+    sarin:84,
+    mahendra:90
+}
+for(i=0;i<Object.keys(marks).length;i++){
+    console.log("The marks of " +Object.keys(marks)[i] +" are "+marks[Object.keys(marks)[i]])
+}
---
+++ Practise Set 3/question2.js
@@ -0,0 +1,18 @@
+// //Write a program in question 1 using for in loop
+// let marks={
+//     harry:98,
+//     rohan:70,
+//     aakash:7
+// }
+// for(let key in marks){
+//     console.log(key+"are"+marks[key])}
+
+let marks={
+    bishal:80,
+    raj:70,
+    sarin:84,
+    mahendra:90
+}
+for(let keys in marks){
+    console.log("The marks of " +keys+ " are " +marks[keys])
+}
